7f6467e9fdbae4abed8fe65132232479a6405d36
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2022-10-27 20:51:52 +0300

    Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,42 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-import * as basicLightbox from "basiclightbox";
 
-const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`);
+const galleryItem = document.querySelector(".gallery");
 
-instance.show();
+galleryItem.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source} width="800" height="600">`
+  );
 
-console.log(galleryItems);
+  instance.show();
+
+  window.addEventListener("keydown", closeGallery);
+  function closeGallery(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+});
+
+function createGallery(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+galleryItem.innerHTML = createGallery(galleryItems);
